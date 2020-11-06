7cee9d8b88d4a8cdc60bd14ce991f01d0659df71
Pranav Gaddamadugu <pranavsaig@gmail.com>
2020-11-05 19:10:56 -0800

    Modified install script to condense data files

--- workloads/azure/scripts/install-dataset.py
+++ workloads/azure/scripts/install-dataset.py
@@ -12,11 +12,11 @@ import tarfile
 TAR_FILENAME = "azurefunctions-dataset2019.tar.xz"
 URL = f"https://azurecloudpublicdataset2.blob.core.windows.net/azurepublicdatasetv2/azurefunctions_dataset2019/{TAR_FILENAME}"
 
-# Download file
-resp = requests.get(URL, allow_redirects=True, stream=True)
-
 tar_path = os.path.join(os.getcwd(), TAR_FILENAME)
 dir_name = TAR_FILENAME.split(".")[0]
+
+# Download file
+resp = requests.get(URL, allow_redirects=True, stream=True)
 
 with open(tar_path, 'wb') as tf:
     tf.write(resp.content)
@@ -31,37 +31,62 @@ os.remove(tar_path)
 # Join and rename files in dataset
 dataset_dir = os.path.join(os.getcwd(), dir_name)
 memory_files = []
-function_duration_files = []
-function_invocation_files = []
+duration_files = []
+invocation_files = []
 for entry in os.scandir(dataset_dir):
     if "app" in entry.path:
         memory_files.append(entry.path)
-    elif "durations" in entry.path:
-        function_duration_files.append(entry.path)
-    elif "invocations" in entry.path:
-        function_invocation_files.append(entry.path)
+    elif "function_durations" in entry.path:
+        duration_files.append(entry.path)
+    elif "invocations_per" in entry.path:
+        invocation_files.append(entry.path)
     else:
         print(f"Unknown file: {entry.path}")
 
 # Do some sorting
-function_files = zip(sorted(function_duration_files), sorted(function_invocation_files))
+duration_files = sorted(duration_files)
+invocation_files = sorted(invocation_files)
 memory_files = sorted(memory_files)
 
-
-# Rename memory files
+# Join the memory files
+cumm_memory = None
 for (i, entry) in enumerate(memory_files):
-     new_path = os.path.join(dataset_dir, f"memory.d{i+1}.csv")
-     os.rename(entry, new_path)
+   csv = pd.read_csv(entry)
+   csv['Day'] = i + 1
+   if cumm_memory is not None:
+       cumm_memory = pd.concat([cumm_memory, csv], ignore_index=True)
+   else:
+       cumm_memory = csv
+   os.remove(entry)
+new_path = os.path.join(dataset_dir, 'memory.csv')
+cumm_memory.to_csv(new_path, index=False)
+
+# Join all invocations
+cumm_invocations = None
+for (i, entry) in enumerate(invocation_files):
+    name_mapping = {f'{min}':f'{i * 1440 + min}' for min in range(1, 1441)}
+    csv = pd.read_csv(entry)
+    csv = csv.rename(columns=name_mapping)
+    if cumm_invocations is not None:
+        cumm_invocations = cumm_invocations.merge(csv, on=['HashOwner', 'HashApp', 'HashFunction', 'Trigger'])
+    else:
+        cumm_invocations = csv
+    os.remove(entry)
+new_path = os.path.join(dataset_dir, 'invocations.csv')
+cumm_invocations.to_csv(new_path, index=False)
 
 # Join the function files
-for (i, (dur, inv)) in enumerate(function_files):
-   dur_csv = pd.read_csv(dur)
-   inv_csv = pd.read_csv(inv)
-   merged = dur_csv.merge(inv_csv, on=['HashOwner', 'HashApp', 'HashFunction'])
-   new_path = os.path.join(dataset_dir, f"functions.d{i+1}.csv")
-   merged.to_csv(new_path, index=False)
-   
-   # Remove old data
-   os.remove(dur)
-   os.remove(inv)
+cumm_durations = None
+for (i, entry) in enumerate(duration_files):
+   csv = pd.read_csv(entry)
+   csv['Day'] = i + 1
+   if cumm_durations is not None:
+       cumm_durations = pd.concat([cumm_durations, csv], ignore_index=True)
+   else:
+       cumm_durations = csv
+   os.remove(entry)
+new_path = os.path.join(dataset_dir, 'durations.csv')
+cumm_durations.to_csv(new_path, index=False)
+
+
 
---
+++ workloads/azure/utils.py
@@ -0,0 +1,5 @@
+"""Utilities for reading and processing the AzureFunctions dataset."""
+import pandas as pd
+
+def read_function_invocations(path, start_time=1, end=20160):
+    return None

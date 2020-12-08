829a506772ed9e44039360ad97b767a77e40ff60
Aditya Ramkumar <ramkumar.aditya@gmail.com>
2020-12-07 22:03:27 -0800

    Added new type of dag

---
+++ workloads/toy/branch_dag.py
@@ -0,0 +1,71 @@
+from simulator.dag import Dag, Function
+from simulator.resource import ResourceType
+from simulator.runtime import ConstantTime
+
+branch_front = Function(
+	unique_id='branch_front',
+	resources= {
+		'STD_CPU' : {
+			'type' : ResourceType.CPU,
+			'space': 100.0,  # Ignoring space this function requires on the CPU
+			'pre'  : ConstantTime(1),
+			'exec' : ConstantTime(3),
+			'post' : ConstantTime(0)
+		},
+		'STD_GPU' : {
+			'type' : ResourceType.GPU,
+			'space': 100.0,
+			'pre'  : ConstantTime(1),
+			'exec' : ConstantTime(1),
+			'post' : ConstantTime(0)
+		}
+	}
+)
+
+branch_middles = [Function(    # This function takes a long time to run on a CPU
+	unique_id='branch_middle_' + str(ind),
+	resources= {
+		'STD_CPU' : {
+			'type' : ResourceType.CPU,
+			'space': 100.0,  # Ignoring space this function requires on the CPU
+			'pre'  : ConstantTime(1),
+			'exec' : ConstantTime(5),
+			'post' : ConstantTime(0)
+		},
+		'STD_GPU' : {
+			'type' : ResourceType.GPU,
+			'space': 100.0,
+			'pre'  : ConstantTime(1),
+			'exec' : ConstantTime(1),
+			'post' : ConstantTime(0)
+		}
+	}
+) for ind in range(10)]
+
+
+
+branch_back = Function(     # This function takes a long time to run on a GPU
+	unique_id='branch_back',
+	resources= {
+		'STD_CPU' : {
+			'type' : ResourceType.CPU,
+			'space': 100.0,  # Ignoring space this function requires on the CPU
+			'pre'  : ConstantTime(1),
+			'exec' : ConstantTime(1),
+			'post' : ConstantTime(0)
+		},
+		'STD_GPU' : {
+			'type' : ResourceType.GPU,
+			'space': 100.0,
+			'pre'  : ConstantTime(1),
+			'exec' : ConstantTime(5),
+			'post' : ConstantTime(0)
+		}
+	}
+)
+
+branch_dag = Dag('branch', funs=[branch_front] + branch_middles + [branch_back])
+for branch_middle in branch_middles:
+	branch_dag.add_edge(branch_front, branch_middle)
+	branch_dag.add_edge(branch_middle, branch_back)
+branch_dag.sanity_check()

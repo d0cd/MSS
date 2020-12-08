40d0373ec03240e7becdcf57aea7550f2ebdb4b1
Pranav Gaddamadugu <pranavsaig@gmail.com>
2020-12-07 22:56:46 -0800

    Added branch_dag

--- workloads/toy/toy_workload.py
+++ workloads/toy/toy_workload.py
@@ -5,7 +5,7 @@ from simulator.dag import Dag
 from simulator.system import System
 
 from workloads.toy.linear_dag import linear_dag
-
+from workloads.toy.branch_dag import branch_dag
 
 class SimpleSystem(System):
 
@@ -57,8 +57,11 @@ class SimpleSystem(System):
 
 cpu_pool = ResourcePool("STD_CPU_POOL", ResourceType.CPU, [("STD_CPU_1", Resource("STD_CPU", ResourceType.CPU))])
 gpu_pool = ResourcePool("STD_GPU_POOL", ResourceType.GPU, [("STD_GPU_1", Resource("STD_GPU", ResourceType.GPU))])
+
+# Add DAGs here
 events = EventQueue([
-	(linear_dag, [(0, None), (0, None), (0, None), (0, None), (1, None), (1, None)])
+	(linear_dag, [(0, None), (0, None), (0, None), (0, None), (1, None), (1, None)]),
+	(branch_dag, [(0, None), (0, None), (0, None), (0, None), (1, None), (1, None)])
 ])
 
 system = SimpleSystem(events,
